b7d36d94fb886c13729f6d9131dbc1747678fa1d
Alex <expresproject@gmail.com>
2021-05-26 22:47:33 +0300

    fixed send form;

--- js/sendForm.js
+++ js/sendForm.js
@@ -19,38 +19,57 @@ export const sendForm = () => {
     request.send(data);
   };
 
-  const dataTest = {
-    name: 'alex',
-    phone: '+3333333333'
-  };
-
   const formElems = document.querySelectorAll('.form');
 
+  const dataTest = {};
+
   const formHandler = (form) => {
+    const smallElem = document.createElement('small');
+    form.append(smallElem);
+
     form.addEventListener('submit', e => {
-      e.preventDefault();
-      const data = {};
+        e.preventDefault();
+        const data = {};
+        let flag = true;
 
-      for(const {name, value} of form.elements) {
-        if(name) {
-          data[name] = value;
+        const buttonSubmit = form.querySelector('.button[type="submit"]');
+        
+        for(const elem of form.elements) {
+          const {name, value} = elem;
+          if(name) {
+            if(value.trim()) {
+              data[name] = value.trim();
+              smallElem.textContent = '';
+            } else {
+              elem.style.border = '1px solid red';
+              flag = false;
+              elem.value = '';
+            }
+          }
         }
-      }
+  
+        if(!flag) {
+          smallElem.style.color = 'red';
+          return smallElem.textContent = 'заполните все поля';
+        }
+  
+        sendData(JSON.stringify(dataTest), 
+        (id) => {
+          smallElem.textContent = 'Ваша заявка №' + id + ' отправлена! \nВ ближайшое время с вами свяжутся!';
+          smallElem.style.color = 'green';
+          buttonSubmit.disabled = true;
 
-      const smallElem = document.createElement('small');
-
-      sendData(JSON.stringify(dataTest), 
-      (id) => {
-        smallElem.textContent = 'Ваша заявка №' + id + ' отправлена! \nВ ближайшое время с вами свяжутся!';
-        smallElem.style.color = 'green';
-        form.append(smallElem);
-      }, 
-      (err) => {
-        smallElem.textContent = 'К сожалению произошла ошибка, попробуйте позже';
-        smallElem.style.color = 'red';
-        form.append(smallElem);
-      });
-      form.reset();
+          setTimeout(() => {
+            smallElem.textContent = '';
+            buttonSubmit.disabled = false;
+          }, 3000);
+        }, 
+        (err) => {
+          smallElem.textContent = 'К сожалению произошла ошибка, попробуйте позже';
+          smallElem.style.color = 'red';
+        });
+        form.reset();
+      
       });
   };
 
